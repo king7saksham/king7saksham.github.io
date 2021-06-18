7bfbfd1a99113163575aca27a2a01a8b0901b7bc
Saksham Soni <57166834+king7saksham@users.noreply.github.com>
2021-06-18 21:42:45 +0530

    Add files via upload

--- HTML5/Location/myLoc.js
+++ HTML5/Location/myLoc.js
@@ -2,7 +2,7 @@ window.onload = getMyLocation;
 
 function getMyLocation () {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(displayLocation);
+    navigator.geolocation.getCurrentPosition(displayLocation, displayError);
   } else {
       alert("No Location Available");
     }
@@ -14,4 +14,20 @@ function displayLocation (position) {
 
   var div = document.getElementById("location");
   div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+}
+
+function displayError (error) {
+  var errorTypes = {
+    0: "Unknown error",
+    1: "Permission denied by user",
+    2: "Position is not available",
+    3: "Request timed out"
+  };
+  
+  var errorMessage = errorTypes[error.code];
+  if(error.code == 0 || error.code == 2) {
+    errorMessage = errorMessage + " " + error.message;
+  }
+  var div = document.getElementById("location");
+  div.innerHTML = errorMessage;
 }
