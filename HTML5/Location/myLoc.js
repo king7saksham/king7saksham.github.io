2acb841831c69663ec90fbf37501560fecd02064
Saksham Soni <saksham.soni.mat19@itbhu.ac.in>
2021-06-19 11:10:32 +0530

    gps

--- HTML5/Location/myLoc.js
+++ HTML5/Location/myLoc.js
@@ -1,8 +1,53 @@
+var ourCoords = {
+  latitude: 47.624851,
+  longitude: -122.52099
+};
+
+var map;
+
+function showMap(coords) {
+  var googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
+
+  var mapOptions = {
+    zoom: 10,
+    center: googleLatAndLong,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var mapDiv = document.getElementById("map");
+  map = new google.maps.Map(mapDiv, mapOptions);
+
+  var title = "Your Location";
+  var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+  addMarker(map, googleLatAndLong, title, content);
+}
+
+function addMarker(map, latlong, title, content) {
+  var markerOptions = {
+    position: latlong,
+    map: map,
+    title: title,
+    clickable: true
+  };
+
+  var marker = new google.maps.Marker(markerOptions);
+
+  var infoWindowOptions = {
+    content: content,
+    position: latlong
+  };
+
+  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+  google.maps.event.addListener(marker, "click", function() {
+      infoWindow.open(map);
+  });
+}
+
 window.onload = getMyLocation;
 
 function getMyLocation () {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(displayLocation);
+    navigator.geolocation.getCurrentPosition(displayLocation, displayError);
   } else {
       alert("No Location Available");
     }
@@ -14,4 +59,44 @@ function displayLocation (position) {
 
   var div = document.getElementById("location");
   div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+
+  var km = computeDistance(position.coords, ourCoords);
+  var distance = document.getElementById("distance");
+  distance.innerHTML = "You are " + km + " km away from WickedlySmart HQ";
+
+  showMap(position.coords);
+}
+
+function displayError (error) {
+  var errorTypes = {
+    0: "Unknown error",
+    1: "Permission denied by user",
+    2: "Position is not available",
+    3: "Request timed out"
+  };
+  
+  var errorMessage = errorTypes[error.code];
+  if(error.code == 0 || error.code == 2) {
+    errorMessage = errorMessage + " " + error.message;
+  }
+  var div = document.getElementById("location");
+  div.innerHTML = errorMessage;
+}
+
+function computeDistance(startCoords, destCoords) {
+  var startLatRads = degreesToRadians(startCoords.latitude);
+  var startLongRads = degreesToRadians(startCoords.longitude);
+  var destLatRads = degreesToRadians(destCoords.latitude);
+  var destLongRads = degreesToRadians(destCoords.longitude);
+
+  var Radius = 6371;
+  var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
+                                             Math.cos(startLatRads) * Math.cos(destLatRads)*
+                                             Math.cos(startLongRads - destLongRads)) * Radius;
+  return distance;
+}
+
+function degreesToRadians (degrees) {
+  var radians = (degrees * Math.PI)/180;
+  return radians;
 }
